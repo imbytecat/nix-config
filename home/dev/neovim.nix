365f5466febd9b55856e679c59f84ae2f2fa0ce1
imbytecat <imbytecat@gmail.com>
2026-04-11 02:36:05 +0800

    fix: 覆盖 catppuccin/nix 的 nvim 包跳过 detect_integrations require check，全量启用 catppuccin

--- home/dev/neovim.nix
+++ home/dev/neovim.nix
@@ -1,6 +1,16 @@
-{ ... }:
+{ inputs, pkgs, ... }:
 
 {
+  # catppuccin/nix's nvim package is missing nvimSkipModule for detect_integrations
+  # Override with higher priority than mkDefault to fix require check failure
+  catppuccin.sources.nvim =
+    (inputs.catppuccin.packages.${pkgs.stdenv.hostPlatform.system}.nvim).overrideAttrs
+      (old: {
+        nvimSkipModule = (old.nvimSkipModule or [ ]) ++ [
+          "catppuccin.lib.detect_integrations"
+        ];
+      });
+
   programs.neovim = {
     enable = true;
     defaultEditor = true;
