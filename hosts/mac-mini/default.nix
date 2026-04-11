7686f7209bf12051703fda43ed6aea3dc750f5d9
imbytecat <imbytecat@gmail.com>
2026-04-11 20:15:31 +0800

    feat(mac-mini): 启用 Wake on LAN，关闭 Power Nap

--- hosts/mac-mini/default.nix
+++ hosts/mac-mini/default.nix
@@ -15,10 +15,15 @@
   power.restartAfterPowerFailure = true;
   power.restartAfterFreeze = true;
 
-  # ── Screen Sharing (VNC) ─────────────────────────
+  # ── Screen Sharing (VNC) & pmset ─────────────────
   system.activationScripts.postActivation.text = ''
+    # VNC
     launchctl enable system/com.apple.screensharing
     launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist 2>/dev/null || true
+    # Wake on LAN
+    pmset -a womp 1
+    # Disable Power Nap
+    pmset -a powernap 0
   '';
 
   system.stateVersion = 5;
