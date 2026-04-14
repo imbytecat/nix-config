27b265803c7f484600dea0364d2a255894e24b53
imbytecat <imbytecat@gmail.com>
2026-04-14 19:33:35 +0800

    feat(darwin): 添加 iPreview Quick Look 扩展

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -85,6 +85,7 @@
 
     # Mac App Store
     masApps = {
+      "iPreview" = 1519213509;
       "Microsoft Word" = 462054704;
       "Microsoft Excel" = 462058435;
       "Microsoft PowerPoint" = 462062816;
