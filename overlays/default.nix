b41a2eec47e5d94408f28e2d26a4ff295aa37bdd
imbytecat <imbytecat@gmail.com>
2026-04-21 22:11:25 +0800

    chore(nix): 更新 flake input 并移除 nushell overlay
    unstable channel 的 nushell 已修复 darwin 编译问题，不再需要从
    master 借包。

--- overlays/default.nix
+++ overlays/default.nix
@@ -21,11 +21,6 @@ inputs.nixpkgs.lib.composeManyExtensions [
     {
       # opencode 发版频繁，跟 master 更新更快
       inherit (master) opencode;
-
-      # https://github.com/NixOS/nixpkgs/pull/510439
-      # unstable channel 的 nushell 在 darwin 上编译失败，PR 已合并 master，
-      # 待 unstable channel 推进后可删除此行。
-      inherit (master) nushell;
     }
   )
 
