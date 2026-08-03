755c353beb6ce259ad58f0476cc24c0f33bda1c0
imbytecat <imbytecat@gmail.com>
2026-08-03 15:59:50 +0800

    chore(pkgs): orca-ide 升到 1.4.165

--- pkgs/orca-ide/default.nix
+++ pkgs/orca-ide/default.nix
@@ -52,11 +52,11 @@
 
 let
   pname = "orca-ide";
-  version = "1.4.163";
+  version = "1.4.165";
 
   src = fetchurl {
     url = "https://github.com/stablyai/orca/releases/download/v${version}/orca-linux.AppImage";
-    hash = "sha256-OWphybstsEMdTSOqqKUcR2+HW1mxWQYj2JFPsnpVIEk=";
+    hash = "sha256-bUfsREvJIArWRdM5n3MIffjec5Zd99HU0AQUKmZJj8s=";
   };
 
   appimageContents = appimageTools.extract { inherit pname version src; };
