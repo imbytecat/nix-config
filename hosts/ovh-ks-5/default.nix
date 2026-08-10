9658cdd7f0490f45a58197a5ca8869337566cec7
imbytecat <imbytecat@gmail.com>
2026-08-10 20:59:49 +0800

    chore(ovh): ovh-ks-5 迁到美国机房

--- hosts/ovh-ks-5/default.nix
+++ hosts/ovh-ks-5/default.nix
@@ -9,7 +9,7 @@
   system.stateVersion = "25.11";
 
   boot = {
-    # 法国到中国的直连 TCP；其余调优需指标证据。
+    # 美国到中国的直连 TCP；其余调优需指标证据。
     kernel.sysctl = {
       "net.core.default_qdisc" = "fq";
       "net.ipv4.tcp_congestion_control" = "bbr";
